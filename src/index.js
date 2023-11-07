4454d29e0acacd359920c65a17cf0ec2acdcea86
SurajJadhav7 <jadhavsuraj777@gmail.com>
2023-11-07 14:03:27 +0530

    added iohook for keylogging

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
-const { app, BrowserWindow } = require('electron');
-const { uIOhook, UiohookKey } = require('uiohook-napi');
+const { app, BrowserWindow, systemPreferences } = require('electron');
 const path = require('path');
+
+function checkAccessibilityPermissions(callback) {
+  // Immediately check if the app is already a trusted accessibility client without prompting the user
+  if (systemPreferences.isTrustedAccessibilityClient(false)) {
+    console.log('The app is already trusted as an accessibility client.');
+    callback(true);
+  } else {
+    // Not trusted, so try with prompting
+    const isTrustedNow = systemPreferences.isTrustedAccessibilityClient(true);
+    if (isTrustedNow) {
+      console.log('We have accessibility permissions now!');
+      callback(true);
+    } else {
+      console.log('The app is not trusted as an accessibility client.');
+      // Start a periodic check every 10 seconds to see if the user has granted access
+      const intervalId = setInterval(() => {
+        if (systemPreferences.isTrustedAccessibilityClient(false)) {
+          console.log('Accessibility permissions granted after checking again.');
+          clearInterval(intervalId);
+          callback(true);
+        } else {
+          console.log('Still not trusted, waiting for user to grant permissions...');
+        }
+      }, 3000);
+    }
+  }
+}
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -22,22 +48,22 @@ const createWindow = () => {
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
-
-  uIOhook.on('keydown', (e) => {
-    console.log('key pressed...', e)
-  });
-  
-  uIOhook.start()
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.whenReady().then(() => {
+  checkAccessibilityPermissions((isTrusted) => {
+    if (isTrusted) {
+      const { uIOhook } = require('uiohook-napi');
+      uIOhook.on('keydown', (e) => {
+        console.log('key pressed...', e)
+      });
+      
+      uIOhook.start();
+      createWindow();
+    }
+  });
+});
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
@@ -45,12 +71,7 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
